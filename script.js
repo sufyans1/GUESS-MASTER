bc5309122f5ece57a553646bf7212e169d65413e
muhammed sufyan <132143104+sufyans1@users.noreply.github.com>
2023-05-08 20:30:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let result =document.getElementById("result");
+let btnGuess =document.getElementById("guess");
+let btnRetry =document.getElementById("retry");
+let resultW = document.getElementById("congrats");
+let rnumber = document.getElementById("rnumber");
+let randomvalue1;
+let i;
+function randomValue()
+{
+
+    i=0;
+    randomvalue1 = Math.floor(Math.random()*101);
+    console.log(randomvalue1);
+    result.innerHTML = "";
+    btnRetry.style.visibility = "hidden";
+    btnGuess.style.visibility = "visible";
+    resultW.style.visibility = "hidden";
+    rnumber.innerHTML = i + "/7";
+
+}
+
+function getValue()
+{
+    let value = document.getElementById("screen");
+    let input = value.value;
+    console.log(input);
+    console.log(randomvalue1);
+
+    if(i < 7)
+    {
+
+        if(input == null || input >100 || input <0)
+        {
+            result.innerHTML = "please enter a valid number";
+        }
+        else
+        {
+            i+=1;
+        if (input == randomvalue1) 
+        {
+            console.log("good");
+            result.style.visibility = "hidden";
+            resultW.style.visibility = "visible";
+            btnRetry.style.visibility = "visible";
+            
+        } 
+        else if(input < randomvalue1)
+        {
+            result.innerHTML = "oops too low ";
+            rnumber.innerHTML = "<br>" +i + "/7";
+        }
+        else if(input > randomvalue1)
+        {
+            result.innerHTML = "oops too high"
+            rnumber.innerHTML = "<br>" +i + "/7";
+        }
+        }
+    }
+    else
+    {
+        result.innerHTML =   "GAME OVER ";
+        btnGuess.style.visibility = "hidden";
+        btnRetry.style.visibility = "visible";
+        
+    }
+
+}
+
+randomValue();
